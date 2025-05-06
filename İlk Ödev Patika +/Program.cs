33cb9c2f8471b6ea5d1791916652b0309e6810d5
Bilal Hantik <bilalhantik@gmail.com>
2025-05-06 19:20:59 +0300

    İlk uygulamam
    İlk uygulamam

---
+++ İlk Ödev Patika +/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("*** İlk Uygulamam ***");
+Console.WriteLine("Bilal Hantik");
+Console.WriteLine("Patika'nin deneyimli kadrosu ve geniş referans ağlari sayesinde yazilim dünyasina hizli bir şekilde giriş yapabileceğim için bu bootcampteyim.");
