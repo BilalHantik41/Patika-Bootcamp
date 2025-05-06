5c27418d4026d7f96150cd0cde1432df8de09368
Bilal Hantik <bilalhantik@gmail.com>
2025-05-06 22:10:56 +0300

    Pratik If Else
    Pratik If Else

---
+++ If Else Uygulama/Program.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+
+namespace Program
+{
+    class Program
+    {
+       public static void Main(string[] args)
+       
+        {
+            
+            Console.WriteLine("Merhaba Lütfen bir sayi giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayi: " + sayi);
+            if (sayi>10)
+            {
+                Console.WriteLine("Girmiş Olduğunuz sayi 10'dan büyüktür.");
+            }
+            else if(sayi<10)
+            {
+                Console.WriteLine("Girmiş Olduğunuz sayi 10'dan küçüktür.");
+            
+            }
+            else if (sayi == 10)
+            {
+                Console.WriteLine("Girmiş olduğunuz sayi 10'a eşittir.");
+            }
+            else{
+            Console.WriteLine("Geçersiz bir değer girdiniz..");
+            }
+             if (sayi%2 == 0)    
+            {   
+                Console.WriteLine("Girmiş olduğunuz sayi çifttir.");
+            }
+            
+
+        }
+       
+    }
+}
--- If Else Uygulama/obj/Debug/net9.0/If Else Uygulama.AssemblyInfo.cs
+++ If Else Uygulama/obj/Debug/net9.0/If Else Uygulama.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Pratik Değişkenler ile Uygulama")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("If Else Uygulama")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa13d85c0819526e46ad635a9298dd5542d1fc6f")]
-[assembly: System.Reflection.AssemblyProductAttribute("Pratik Değişkenler ile Uygulama")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Pratik Değişkenler ile Uygulama")]
+[assembly: System.Reflection.AssemblyProductAttribute("If Else Uygulama")]
+[assembly: System.Reflection.AssemblyTitleAttribute("If Else Uygulama")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
