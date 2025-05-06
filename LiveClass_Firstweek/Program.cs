c9fe273d2f38132ad47c61f3f551893527fec15e
Bilal Hantik <bilalhantik@gmail.com>
2025-05-06 21:30:10 +0300

    Değişkenler ile Uygulama
    Değişkenler ile Uygulama

---
+++ LiveClass_Firstweek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya");
+            System.Console.WriteLine("Benim Adim Bilal");
+            System.Console.WriteLine("Hoşçakal");
+
+            System.Console.WriteLine("-------------------");
+
+            System.Console.Write("Merhaba Dünya");
+            System.Console.Write("  ");
+            System.Console.WriteLine("Benim adim Bilal");
+        }
+    }
+}
---
+++ Pratik Değişkenler ile Uygulama/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Lütfen aşağidaki bilgileri giriniz: ");
+            Console.WriteLine("T.C Kimlik Numaranizi Giriniz: ");
+            int TC_Kimlik_Numarasi= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Adinizi Giriniz: ");
+            string ad = Console.ReadLine();
+
+            Console.WriteLine("Soyadinizi Giriniz: ");
+            string Soyad = Console.ReadLine();
+
+            Console.WriteLine("Telefon Numaranizi Giriniz: ");
+            int Telefon = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Yaşinizi Giriniz: ");
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("İlk Aldiğiniz ürün fiyatini giriniz: ");
+            int ürün = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("İkinci aldiğiniz ürünün fiyatini giriniz: ");
+            int ürün2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("*****Girmiş Olduğunuz bilgiler *****");
+            Console.WriteLine("Adiniz: " + ad);
+            Console.WriteLine("Soyadiniz: " + Soyad);
+            Console.WriteLine("Telefon: " + Telefon);
+            Console.WriteLine("Yaşiniz: " + yas);
+            Console.WriteLine("İlk aldiğiniz ürün fiyati: " + ürün);
+            Console.WriteLine("İkinci aldiğiniz ürün fiyati: " +ürün2);
+
+
+
+        }
+    }
+}
